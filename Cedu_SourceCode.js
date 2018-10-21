a08937cf7e54c928932850dd2011b2fe13155411
gibiee <gibiee@naver.com>
2018-10-21 19:47:39 +0900

    source function

--- Cedu_SourceCode.js
+++ Cedu_SourceCode.js
@@ -1,1 +1,18 @@
 //활용할 방법이 없나...
+function useTab(e)
+{
+  var keyCode = e.keyCode || e.which;
+  if (keyCode == 9) {
+    e.preventDefault();
+    var start = this.selectionStart;
+    var end = this.selectionEnd;
+    var text = $(this).val();
+    var selText = text.substring(start, end);
+    $(this).val(
+      text.substring(0, start) +
+      "    " + selText.replace(/\n/g, "\n\t") +
+      text.substring(end)
+    );
+    this.selectionStart = this.selectionEnd = start + 4;
+  }
+}
