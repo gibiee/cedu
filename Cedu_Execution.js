6a1c5e73dd5ace24acae96119542b8005c769236
gibiee <gibiee@naver.com>
2018-11-05 22:07:18 +0900

    null_check() 함수 도입과 컴파일 단계에서 고정 자료형 체크

--- Cedu_Execution.js
+++ Cedu_Execution.js
@@ -2,27 +2,23 @@ function execution(source_code) {
 
   $("#출력").text('').css('color','black'); //출력박스 초기화
 
-  source_code = change_code_input(source_code, 입력창_value);
-  if( source_code == false ) return false;
+  //source_code = change_code_input(source_code, 입력창_value);
 
-  //고정 자료형 체크
-  var 변수들_type = get_variables_type(source_code); //선언된 변수들의 자료형 저장
-  if( equal_type_checking(source_code, 변수들_type) == false )  return false;
-
-  //출력 중 undefined 체크
-  source_code = check_출력_undefined(source_code);
+  source_code = check_출력_undefined(source_code);  //출력 중 undefined 체크
 
   //Cedu 코드를 JavaScript로 바꾸기
   source_code = change_code_for(source_code);
   source_code = change_code(source_code);
 
-  console.log(source_code);
   try {
     var err_value;
     var err_lineNo;
+    console.log(source_code);
     eval(source_code);
   }
   catch(e) {
+    if($('#출력').text() != "") $("#출력").append('\n');
+
     if(e == 'Undefined') {
       var result = "<span style='color:red'>오류 : 변수 " + err_value + "의 출력은 값이 없습니다.\n</span>";
       $("#출력").append(result);
@@ -36,141 +32,6 @@ function execution(source_code) {
         var result = "<span style='color:red'>오류 : " + err_lineNo +"번째 줄에서 예기치 않은 오류 발생!\n</span>"
       }
       $("#출력").append(result);
-    }
-  }
-  return true;
-}
-
-function change_code_input(source_code, 입력창_value) {
-  var start = -1, in_start;
-  var end;
-  var in_variable = [];
-  var index = 0;
-  var str, temp_substr;
-  var count = 0;
-
-  while (true) {
-    start = source_code.indexOf('입력(', start+1);
-    if(start == -1) break;
-    in_start = start + 3;
-    end = source_code.indexOf(')', start);
-
-    in_variable = source_code.substring(in_start,end).split(',');
-    count += in_variable.length;
-    str = '';
-    for(var i in in_variable) {
-      in_variable[i] = in_variable[i].replace(/^\s*/g, '').replace(/\s*$/g, '');
-      if(typeof 입력창_value[index] == "number") { str += in_variable[i] + " = " + 입력창_value[index++] + ";"; }
-      else { str += in_variable[i] + " = " + "'" + 입력창_value[index++] + "'" + ";"; }
-    }
-    end = source_code.indexOf(';', end);
-    temp_substr = source_code.substring(start, end+1);
-    source_code = source_code.replace(temp_substr, str);
-  }
-  if(count > 0 )
-  {
-    if(입력창_value[0] == "") {
-      var result = "<span style='color:red'>오류 : 입력 값이 충분하지 않습니다.\n</span>";
-      $('#출력').append(result);
-      return false;
-    }
-    else if(입력창_value.length != count) {
-      var result = "<span style='color:orange'>경고 : 입력 받는 변수와 입력 값의 수가 서로 다릅니다.\n</span>";
-      $('#출력').append(result);
-    }
-  }
-  return source_code;
-}
-
-function get_variables_type(source_code) {
-  var 변수종류 = [ '변수', '실수', '정수', '문자', '문자열' ];
-  var start = -1 , end;
-  var end_1, end_2;
-  var type = {}; //객체배열
-  var index = 0;
-  var str;
-
-  for(var i in 변수종류) {
-    while(true) {
-      start = source_code.indexOf(변수종류[i] + ' ', start+1);
-      if(start == -1) break;
-      start = source_code.indexOf(' ', start);
-
-      end_1 = source_code.indexOf(';',start);
-      if( end_1 == -1 ) end_1 = Infinity;
-      end_2 = source_code.indexOf('=',start);
-      if( end_2 == -1 ) end_2 = Infinity;
-      end = Math.min(end_1, end_2);
-
-      str = source_code.substring(start,end).split(',');
-      for(var j in str) {
-        str[j] = str[j].replace(/\s/g,'');
-        type[str[j]] = 변수종류[i];
-      }
-    }
-  }
-  return type;
-}
-
-function equal_type_checking(source_code, 변수들_type)
-{
-  var equal = -1;
-  var start, end;
-  var can_break = false;
-  var left_type, right_type;
-
-  while(true) {
-    equal = source_code.indexOf('=', equal+1);
-    if( equal == -1 ) break;
-    else if( source_code[equal+1] == '=' || source_code[equal-1] == '=' || source_code[equal-1] == '!') continue; //==, != 인 경우
-    else if( source_code[equal-1] == '>' || source_code[equal-1] == '<') continue; //>=, <= 인 경우
-
-    can_break = false;
-    for(start = equal - 1; start >= 0; start--) {
-      if( /\s/.test(source_code[start]) == false ) { can_break = true; }
-      if(can_break == true && /[{}();]/.test(source_code[start]) ) { break; }
-    }
-    can_break = false;
-    for(end = equal + 1; end <= source_code.length; end++) {
-      if( /\s/.test(source_code[end]) == false ) { can_break = true; }
-      if(can_break == true && source_code[end] == ';' ) { break; }
-    }
-    var str = source_code.substring(start+1,end);
-    str = str.replace(/^\s*/,'').replace(/\s*$/,'').split(/\s*=\s*/);
-
-    left_type = 변수들_type[str[0]];
-    right_type = 변수들_type[str[1]];
-
-    if(left_type == undefined) {  //왼쪽이 선언된 변수가 아닐 때
-      var result = "<span style='color:red'>오류 : 변수 " + str[0] + "의 선언 여부를 확인해주세요.</span>\n"
-      $('#출력').append(result);
-      return false;
-    }
-    else if(right_type == undefined){ //오른쪽이 변수가 아닐 때
-      right_type = type_checking(str[1].replace(/'/g,''));
-
-      if(left_type == "변수") continue;
-      else if(left_type == right_type) continue;
-      else if(left_type == '실수' && right_type == '정수') continue;
-      else if(left_type == '문자열' && right_type == '문자') continue;
-      else {
-        if(left_type == '정수' && str[1].indexOf('정수(') != -1) continue;
-        if(left_type == '문자' && str[1].indexOf('[') != -1 && str[1].indexOf(']') != -1 ) continue;
-
-        var result = "<span style='color:red'>오류 : 변수 " + str[0] + "의 자료형에 " + str[1] + "의 값을 넣을 수 없습니다.</span>\n";
-        $('#출력').append(result);
-        return false;
-      }
-    }
-    else { //오른쪽 또한 변수일 때
-      if(left_type == right_type) continue;
-      else if(left_type == '실수' && right_type == '정수') continue;
-      else if(left_type == '문자열' && right_type == '문자') continue;
-      else {
-        var result = "<span style='color:red'>오류 : 변수 " + str[0] + "와 변수 " + str[1] + "의 자료형을 서로 확인하십시오.</span>\n";
-        $('#출력').append(result);
-        return false;
-      }
     }
   }
   return true;
@@ -193,7 +54,7 @@ function check_출력_undefined(source_code) {
     isUndefined = '';
     for (var i in value) {
       content = value[i].replace(/\s/g,'');
-      isUndefined += " null_check(" + content + ");";
+      isUndefined += " null_check(" + content + ", '" + content + "');";
     }
     end = source_code.indexOf(';', end);
     source_code = source_code.substring(0,end+1) + isUndefined + source_code.substring(end+1, source_code.length);
@@ -245,7 +106,7 @@ function change_code(source_code) {
   source_code = source_code.replace(/줄바꿈\(\);/g, "$('#출력').append('\\n');");
 
   source_code += "\n메인();";
-  source_code += "\n\nfunction null_check(value) {\n if(value == undefined || value == null) {\n err_value = String(value);\n throw 'Undefined'\n }\n}";
+  source_code += "\n\nfunction null_check(value,str) {\n if(value == undefined || value == null) {\n err_value = str;\n throw 'Undefined'\n }\n}";
 
   return source_code;
 }
