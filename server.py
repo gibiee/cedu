051415f878ca9ce03c00fdf99446e92542838e2e
gibiee <gibiee@naver.com>
2019-08-10 01:55:20 +0900

    정리

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
-# app = Flask(__name__)
-app = Flask(__name__, static_folder="templates/css")
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+# app = Flask(__name__, static_folder="templates/css")
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/main.html')
+def main() :
+    return render_template('main.html')
+
+@app.route('/editor.html')
+def editor() :
+    return render_template('editor.html')
+
+@app.route('/question_list.html')
+def question_list() :
+    return redirect(url_for('main'))
 
 if __name__ == '__main__':
     app.run()
